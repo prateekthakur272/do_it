60ab73ce82642e450a397b777c03473b7bc56509
Prateek Thakur <prateekthakur272@gmail.com>
2023-11-09 20:39:28 +0530

    adds material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
-void main(List<String> args) {}
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Do It',
+      home: Placeholder(),
+    );
+  }
+}
+
+
+void main(List<String> args) {
+  runApp(const App());
+}
